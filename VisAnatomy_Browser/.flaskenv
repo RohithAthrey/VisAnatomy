3023fc59fb7f80c651a538abd578b873b3ced6cc
Chen Chen <cchen24@umd.edu>
2024-07-11 15:39:59 -0400

    remove browser folders

--- VisAnatomy_Browser/.flaskenv
+++
@@ -1,3 +0,0 @@
-FLASK_APP=EIDInterface.py
-FLASK_DEBUG=1
-FLASK_ENV=development
--- VisAnatomy_Browser/app/modifycollections.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./AnnotationRecord.csv')
-df['Tag'] = df['Type'].str.split().str[0].str.lower()
-df['Filename'] = df['Type'].str.replace(' ', '') + df['ID'].astype(str) + '.png'
-df['Description'] = df['Type'] + ' ' + df['ID'].astype(str)
-df = df[['Type', 'ID', 'Description', 'Filename', 'Tag', 'Link']]
-
-df.to_csv('examples_collection_output.csv', index=False)
